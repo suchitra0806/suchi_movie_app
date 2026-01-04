67ef5a45f447b71e2580b9623243e34ad2c8b963
Suchitra Rajkumar <suchiraj0806@gmail.com>
2026-01-03 18:26:42 -0600

    fetch and display movie details

--- services/useFetch.ts
+++ services/useFetch.ts
@@ -6,7 +6,7 @@
 import { useEffect, useState } from "react";
 
 
-const useFetch = <T>(fetchFunction: () => Promise<T>, autoFetch: true) => {
+const useFetch = <T>(fetchFunction: () => Promise<T>, autoFetch: boolean = true) => {
     const [data, setData] = useState<T | null>(null);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState<Error | null>(null);
@@ -35,7 +35,7 @@ const useFetch = <T>(fetchFunction: () => Promise<T>, autoFetch: true) => {
         if (autoFetch) {
             fetchData();
         }
-    }, []);
+    }, [autoFetch]);
 
     return { data, loading, error, refetch: fetchData, reset }; // hooks has to return something
 }
