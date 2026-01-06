c5595e92b9bb36d1839c4d5ecdb8b756f8727952
Suchitra Rajkumar <suchiraj0806@gmail.com>
2026-01-05 22:32:50 -0600

    developing the search bar

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,19 +1,33 @@
-import { icons } from '@/constants/icons';
-import React from 'react';
-import { Image, TextInput, View } from 'react-native';
+import { Image, TextInput, View } from "react-native";
+
+import { icons } from "@/constants/icons";
 
 interface Props {
   placeholder: string;
-  onPress: () => void;
+  value?: string;
+  onChangeText?: (text: string) => void;
+  onPress?: () => void;
 }
 
-const SearchBar = ({placeholder, onPress} : Props) => {
+const SearchBar = ({ placeholder, value, onChangeText, onPress }: Props) => {
   return (
     <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
-      <Image source={icons.search} className="size-5" resizeMode="contain" tintColor="#ab8bff" />
-      <TextInput onPress={onPress} placeholder={placeholder} value="" onChangeText={() => {}} placeholderTextColor="#a8b5db" className='flex-1 ml-2 text-white'/>
+      <Image
+        source={icons.search}
+        className="w-5 h-5"
+        resizeMode="contain"
+        tintColor="#AB8BFF"
+      />
+      <TextInput
+        onPress={onPress}
+        placeholder={placeholder}
+        value={value}
+        onChangeText={onChangeText}
+        className="flex-1 ml-2 text-white"
+        placeholderTextColor="#A8B5DB"
+      />
     </View>
-  )
-}
+  );
+};
 
-export default SearchBar
+export default SearchBar;
